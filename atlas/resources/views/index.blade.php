cc9d668a2d80251c4bf8f1f8bab5d44bbd847a3d
CapitanPulgarin <kaputbutton@gmail.com>
2024-02-18 21:19:38 -0500

    V1

---
+++ atlas/resources/views/index.blade.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset('index.css')}}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="arriba">
+        <h1>ATLAS</h1>
+        <h3>Relajacion absoluta</h3> <br>
+
+        <img src="img/foto2.jpg" alt="fotoPerfil">
+        <h4>Carlos</h4>
+    </div>
+    <i class="fa-solid fa-list"></i>
+    <h2>SERVICIOS</h2>
+
+<div class="principal">    
+    <div class="caja">
+        <img src="img/foto.jpg" alt="">
+        <p>SAUNA/SPA <br><br>“¿Tu jefe te está sacando canas?<br> ¡Nosotros te las quitamos!”</p>
+        <button a href="#"></a>Opinar</button>
+        <button a href="#"></a>Reservar</button>
+    </div>   
+    <div class="caja2">
+        <img src="img/ave.jpeg" alt="">
+        <p>... <br><br>“¿Tu jefe te está sacando canas?<br> ¡Nosotros te las quitamos!”</p>
+        <button a href="#"></a>Opinar</button>
+        <button a href="#"></a>Reservar</button>
+    </div>   
+    <div class="caja3">
+        <img src="img/foto.jpg" alt="">
+        <p>... <br><br>“¿Tu jefe te está sacando canas?<br> ¡Nosotros te las quitamos!”</p>
+        <button a href="#"></a>Opinar</button>
+        <button a href="#"></a>Reservar</button>
+    </div>   
+    <div class="caja4">
+        <img src="img/foto.jpg" alt="">
+        <p>... <br><br>“¿Tu jefe te está sacando canas?<br> ¡Nosotros te las quitamos!”</p>
+        <button a href="#"></a>Opinar</button>
+        <button a href="#"></a>Reservar</button>
+    </div>
+</div>
+   
+
+            
+</body>
+</html>
